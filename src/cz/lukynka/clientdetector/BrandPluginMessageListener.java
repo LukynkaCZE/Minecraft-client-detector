136f1614e4b94e71c10543fa678458e312e21cfd
LukynkaCZE <48604271+LukynkaCZE@users.noreply.github.com>
2021-03-27 00:47:56 +0100

    Update BrandPluginMessageListener.java

--- src/cz/lukynka/clientdetector/BrandPluginMessageListener.java
+++ src/cz/lukynka/clientdetector/BrandPluginMessageListener.java
@@ -11,10 +11,10 @@ public class BrandPluginMessageListener implements PluginMessageListener {
     @Override
     public void onPluginMessageReceived(String channel, Player p, byte[] msg) {
         try {
-            Bukkit.getServer().getConsoleSender().sendMessage(ChatColor.GRAY +"[DEV] " +ChatColor.GOLD +p.getDisplayName() +ChatColor.YELLOW +" se pøipojil/a pøes " +ChatColor.AQUA +new String(msg, "UTF-8").substring(1));
+            Bukkit.getServer().getConsoleSender().sendMessage(ChatColor.GRAY +"[DEV] " +ChatColor.GOLD +p.getDisplayName() +ChatColor.YELLOW +" has joined using " +ChatColor.AQUA +new String(msg, "UTF-8").substring(1));
             for(Player all : Bukkit.getServer().getOnlinePlayers()) {
             	if(all.isOp()) {
-            		all.sendMessage(ChatColor.GRAY +"[DEV] " +ChatColor.GOLD +p.getDisplayName() +ChatColor.YELLOW +" se pøipojil/a pøes " +ChatColor.AQUA +new String(msg, "UTF-8").substring(1));
+            		all.sendMessage(ChatColor.GRAY +"[CLIENT DETECTOR] " +ChatColor.GOLD +p.getDisplayName() +ChatColor.YELLOW +" has joined using " +ChatColor.AQUA +new String(msg, "UTF-8").substring(1));
             	}
             }
         } catch (UnsupportedEncodingException e) {
